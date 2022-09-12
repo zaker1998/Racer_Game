b5a58a1b5dd236ee70f44a8f2fc404724a121a60
zaker1998 <70671367+zaker1998@users.noreply.github.com>
2022-09-12 17:23:25 +0200

    new Fields(ROADSIDE_WIDTH,CENTER_X)+new Methods(createGame,drawScene/Field) added!

--- src/com/javarush/games/racer/RacerGame.java
+++ src/com/javarush/games/racer/RacerGame.java
@@ -4,10 +4,26 @@ import com.javarush.engine.cell.*;
 public class RacerGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int ROADSIDE_WIDTH = 14;
+    public static final int CENTER_X = WIDTH/2;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
         showGrid(false);
     }
+    private void createGame(){
+        drawScene();
+    }
+
+    private void drawScene(){
+        drawField();
+    }
+
+    private void drawField(){
+
+    }
+
+
 }
